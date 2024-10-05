f9465f27d187449a9574ae674e2519a380756690
Sathishkumar R <rsathishtechit@gmail.com>
2024-10-05 16:30:32 +0530

    Added replicas for workflow service

--- apps/workflows-service/src/workflows/workflows.service.ts
+++ apps/workflows-service/src/workflows/workflows.service.ts
@@ -1,11 +1,12 @@
 import { CreateWorkflowDto, UpdateWorkflowDto } from '@app/workflows';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Workflow } from './entities/workflow.entity';
 
 @Injectable()
 export class WorkflowsService {
+  private readonly logger = new Logger(WorkflowsService.name);
   constructor(
     @InjectRepository(Workflow)
     private readonly workflowsRepository: Repository<Workflow>,
